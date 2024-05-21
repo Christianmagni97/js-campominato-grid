487e68fcca4b640f62d4b91b8b2f124d49cff1bd
Christianmagni97 <christianmagni28@gmail.com>
2024-05-21 14:34:00 +0200

    problemi in js

---
+++ script.js
@@ -0,0 +1,9 @@
+const baseCellsJs = document.querySelector('section#base-cells');
+
+
+for (let index = 0; index < 100; index++) {
+    const createCells = document.createElement('article');
+    createCells.classList.add('cells');
+    
+}
+baseCellsJs.appendChild(createCells);
