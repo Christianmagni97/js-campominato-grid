3bf657855d877a54c3dcc9f06d879d7e6e54db4c
Christianmagni97 <christianmagni28@gmail.com>
2024-05-23 00:26:27 +0200

    aggiunto righe celle

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const baseCellsJs = document.querySelector('section#base-cells');
 for (let index = 0; index < 100; index++) {
     const createCells = document.createElement('article');
     createCells.classList.add('cells');
+    baseCellsJs.appendChild(createCells);
     
 }
-baseCellsJs.appendChild(createCells);
